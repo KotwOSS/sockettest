5cda1cd0dd43873bb1d717ff9064d3f6a0167756
KekOnTheWorld <kekontheworld@gmail.com>
2022-03-06 18:12:39 +0100

    Added cli subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-// Note: this requires the `derive` feature
-
-use std::ffi::OsString;
 use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
 
-/// A fictional versioning CLI
 #[derive(Parser)]
-#[clap(name = "git")]
-#[clap(about = "A fictional versioning CLI", long_about = None)]
+#[clap(name = "sockettest", about = "A socket tester for udp and tcp.", long_about = None)]
 struct Cli {
     #[clap(subcommand)]
     command: Commands,
@@ -16,46 +11,82 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// Clones repos
+    /// Sends packets to a socket.
     #[clap(arg_required_else_help = true)]
-    Clone {
-        /// The remote to clone
+    Send {
+        /// The type (udp/tcp)
+        socket_type: String,
+
+        /// The remote in format host:port
         remote: String,
+
+        /// The data in form of a string
+        #[clap(long, short, required = false)]
+        data: String,
+
+        /// The file which will be sent
+        #[clap(long, short, required = false)]
+        file: PathBuf,
+
+        /// Amount of packets that will be sent
+        #[clap(long, short, required = false)]
+        amount: u16
     },
-    /// pushes things
+
+    /// Connects to a socket (only tcp).
     #[clap(arg_required_else_help = true)]
-    Push {
-        /// The remote to target
+    Connect {
+        /// The remote in format host:port
         remote: String,
+
+        /// The max time for connecting until it will timeout.
+        #[clap(long, short, required = false)]
+        timeout: u8,
     },
-    /// adds things
+
+    /// Sends packets to a socket and waits until it gets a response
     #[clap(arg_required_else_help = true)]
-    Add {
-        /// Stuff to add
-        #[clap(required = true, parse(from_os_str))]
-        path: Vec<PathBuf>,
+    Wait {
+        /// The type (udp/tcp)
+        socket_type: String,
+
+        /// The remote in format host:port
+        remote: String,
+
+        /// The data in form of a string
+        #[clap(long, short, required = false)]
+        data: String,
+
+        /// The file which will be send
+        #[clap(long, short, required = false)]
+        file: PathBuf,
+
+        /// The max time for waiting until it will timeout.
+        #[clap(long, short, required = false)]
+        timeout: u8,
+
+        /// If true, only the response text will be printed.
+        #[clap(long, short)]
+        silent: bool,
+
+        /// Amount of packets that will be sent
+        #[clap(long, short, required = false)]
+        amount: u16
     },
-    #[clap(external_subcommand)]
-    External(Vec<OsString>),
 }
 
 fn main() {
     let args = Cli::parse();
 
     match &args.command {
-        Commands::Clone { remote } => {
-            println!("Cloning {}", remote);
-        }
-        Commands::Push { remote } => {
-            println!("Pushing to {}", remote);
-        }
-        Commands::Add { path } => {
-            println!("Adding {:?}", path);
-        }
-        Commands::External(args) => {
-            println!("Calling out to {:?} with {:?}", &args[0], &args[1..]);
-        }
-    }
+        Commands::Send { socket_type, remote, data, file, amount } => {
+            
+        },
+        Commands::Wait { socket_type, remote, data, file, amount, timeout, silent } => {
 
-    // Continued program logic goes here...
+        },
+        Commands::Connect { remote, timeout } => {
+
+        },
+    }
 }
